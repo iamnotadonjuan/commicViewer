16b82c29e608b32d20fdbc73f1421ea1f8447bed
Juan Camargo <juancamargo93@outlook.com>
2018-12-21 07:55:38 -0500

    Create cards to display comics & implement infiniteScroll to retrieve more comics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,9 @@ import { HomeService } from './home.service';
 })
 export class HomePage {
 
+  comics = []
+  isLoading = false
+  offset = 0
   constructor(private homeService: HomeService) {
   }
 
@@ -16,12 +19,33 @@ export class HomePage {
   }
 
   retrieveComics () {
+    this.isLoading = true
     this.homeService.getCommics()
       .subscribe((data) => {
-        console.log(data['data'].results)
+        this.isLoading = false
+        if (data['data'].results.length > 0) {
+          this.comics = data['data'].results
+        } else {
+          console.log('Not comics found')
+        }
       }, err => {
+        this.isLoading = false
         console.log('err', err)
       })
   }
 
+  doInfinite (event) {
+    this.offset += 50
+    this.homeService.getCommics(this.offset)
+      .subscribe((data) => {
+        if (data['data'].results.length > 0) {
+          data['data'].results.map(c => this.comics.push(c))
+          event.target.complete()
+        } else {
+          console.log('Not comics found')
+        }
+      }, err => {
+        console.log('err', err)
+      })
+  }
 }
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -6,13 +6,12 @@ import { environment } from "src/environments/environment";
 export class HomeService {
   // urlBase = `https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&apikey=${environment.apiPublicKey}`
 
-  offset = 50
-  urlBase = `http://gateway.marvel.com/v1/public/comics?ts=1&apikey=${environment.apiPublicKey}&hash=${environment.apiHashKey}&limit=50&offset=${this.offset}`
+  urlBase = 'http://gateway.marvel.com/v1/public/comics'
   
   constructor(private http: HttpClient) {
   }
 
-  getCommics () {
-    return this.http.get(this.urlBase)
+  getCommics (offset = 0)  {
+    return this.http.get(`${this.urlBase}?ts=1&apikey=${environment.apiPublicKey}&hash=${environment.apiHashKey}&limit=50&offset=${offset}`)
   }
 }
